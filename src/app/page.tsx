d86d18b46084dd842b334da2506a9ffe360e457c
AlexAirCG <108254394+AlexAirCG@users.noreply.github.com>
2024-04-30 12:59:49 +0300

    add proday-4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+const client = new PrismaClient();
+
+export default async function Home() {
+  const courses = await client.course.findMany();
+
+  console.log(courses);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>button</Button>
